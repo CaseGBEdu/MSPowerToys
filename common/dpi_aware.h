f26fdc0c2cb35299df1d0152a47b12a5cffd6882
casegb <100475182+casegb@users.noreply.github.com>
2022-02-26 16:10:28 +0300

    Add files via upload

---
+++ common/dpi_aware.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "windef.h"
+
+class DPIAware {
+private:
+  static const int DEFAULT_DPI = 96;
+
+public:
+  static HRESULT GetScreenDPIForWindow(HWND hwnd, UINT & dpi_x, UINT & dpi_y);
+  static HRESULT GetScreenDPIForPoint(POINT p, UINT& dpi_x, UINT& dpi_y);
+  static void Convert(HMONITOR monitor_handle, int &width, int &height);
+};
